fd5732fd6bc0ffb821b06a6dd57e3843e90bf3b7
Gianni <gianniicherry@gmail.com>
2022-09-23 08:56:29 -0700

    commit tree 912e4dd191807b669a101f06d5f2c4e6b65ca0e0

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+const employee = {
+    name: "Peacock Johnson",
+    streetAddress: "123 stardew Valley"
+}
+function updateEmployeeWithKeyAndValue(employee, name, value){
+    const  newEmployee = {...employee}
+    newEmployee[name] = value
+    return newEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,name,value){
+   employee[name] = value
+   return employee
+}
+
+function deleteFromEmployeeByKey(employee,name){
+    const newObj = {...employee}
+    delete newObj[name]
+    return newObj
+}
+function destructivelyDeleteFromEmployeeByKey(employee,name){
+    delete employee[name]
+    return employee
+}
